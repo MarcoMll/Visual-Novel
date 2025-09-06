d20eee82b9078624ae315889586aeef2c513d1d4
Marco <marcomalliani@gmail.com>
2025-09-06 19:34:54 +0300

    feat: add extensible runtime save data system

---
+++ Assets/Scripts/Game flow/SaveSystem/BaseGameData.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+
+namespace VisualNovel.GameFlow.SaveSystem
+{
+    /// <summary>
+    /// Base class for any game data section that can be saved or loaded.
+    /// Extend this class to include additional data containers.
+    /// </summary>
+    [Serializable]
+    public abstract class BaseGameData
+    {
+        /// <summary>
+        /// Unique key used to store this data section.
+        /// </summary>
+        protected abstract string SaveKey { get; }
+
+        /// <summary>
+        /// Saves the current state of the data section.
+        /// </summary>
+        public virtual void Save()
+        {
+            var json = JsonUtility.ToJson(this);
+            PlayerPrefs.SetString(SaveKey, json);
+        }
+
+        /// <summary>
+        /// Loads the saved state of the data section.
+        /// </summary>
+        public virtual void Load()
+        {
+            if (!PlayerPrefs.HasKey(SaveKey)) return;
+            var json = PlayerPrefs.GetString(SaveKey);
+            JsonUtility.FromJsonOverwrite(json, this);
+        }
+    }
+}
---
+++ Assets/Scripts/Game flow/SaveSystem/GameDataManager.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VisualNovel.GameFlow.SaveSystem
+{
+    /// <summary>
+    /// Manages saving and loading of the different game data sections.
+    /// </summary>
+    public class GameDataManager : MonoBehaviour
+    {
+        [SerializeReference] private List<BaseGameData> dataSections = new();
+
+        /// <summary>
+        /// Saves all registered data sections.
+        /// </summary>
+        public void SaveGame()
+        {
+            foreach (var section in dataSections)
+            {
+                section.Save();
+            }
+        }
+
+        /// <summary>
+        /// Loads all registered data sections.
+        /// </summary>
+        public void LoadGame()
+        {
+            foreach (var section in dataSections)
+            {
+                section.Load();
+            }
+        }
+    }
+}
